537a3864e474517b7a5e232bd639b4759b88b660
Ishani Roy Chowdhury <roychoii@mail.uc.edu>
2024-04-02 11:26:31 -0400

    added a constructor to the new class named after my 6+2

---
+++ PrimeNumberFun/roychoii.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    internal class roychoii
+    {
+        // add a constructor that prints "Hello"
+        public roychoii()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
